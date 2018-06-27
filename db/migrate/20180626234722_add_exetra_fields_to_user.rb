865c3b9310a3bf9c7b94a2f20425d3cc8bb6b7a1
daisuke <dsaito85a@gmail.com>
2018-06-27 09:04:13 +0900

    ユーザープロフィールの表示、編集機能を追加

---
+++ db/migrate/20180626234722_add_exetra_fields_to_user.rb
@@ -0,0 +1,7 @@
+class AddExetraFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :description, :string
+  end
+end
