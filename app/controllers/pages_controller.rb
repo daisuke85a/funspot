2d6d9e803739d4298de0fc14cfa90449f33d599e
daisuke <dsaito85a@gmail.com>
2018-07-13 10:21:19 -0400

    indexからsearchページに飛べるようにする

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def index
     @users = User.all
   end
+
+  def search
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root :to => 'pages#index'
   resources :users, only: [:show]
 
+get '/search' => 'pages#search'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
