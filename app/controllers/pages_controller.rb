73b7a983b19d69af957f14645e7c40a2c9230d1f
daisuke <dsaito85a@gmail.com>
2018-07-17 23:19:28 -0400

    希望日時が予約済みの場合に検索から外すよう修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,11 +21,36 @@ class PagesController < ApplicationController
 
       #検索結果が空欄の場合  
     else
-      @funspots = Myfunspots.all
+      @funspots = Myfunspot.all
       @latitude = @funspots.to_a[0].latitude
       @longitude = @funspots.to_a[0].longitude
     end
 
+    #funspotデータを配列にしてまとめる
+    @arrfunspots = @funspots.to_a
+
+    if( !params[:start].blank? &&!params[:end].blank? )
+
+      start_date = Date.parse(params[:start])
+      end_date = Date.parse(params[:end])
+
+      @funspots.each do |funspot|
+
+        #予約済みでないかをチェックする
+        unavailable = funspot.reservations.where(
+          "(? <= start AND start <= ?)
+           OR (? <= end AND end <= ?)
+           OR (start < ? AND ? < end)",
+           start_date, end_date,
+           start_date, end_date,
+           start_date, end_date,
+        ).limit(1)
+        
+        # delete unavalable funspot from @funspots
+        if unavailable.length > 0
+          @arrfunspots.delete(funspot)
+        end
+      end
+    end
   end
-  
 end
